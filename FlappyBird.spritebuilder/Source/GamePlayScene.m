ac45eaa01501dfebbd8158d93d9b6f44a94ddf3e
Meg911 <magreth.degraaf@y7mail.com>
2014-10-24 10:05:35 +1000

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -4,16 +4,30 @@
 
 @implementation GamePlayScene
 
-- (void)initialize
-{
-    // your code here
+- (void)initialize{
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
 }
 
--(void)update:(CCTime)delta
-{
+-(void)update:(CCTime)delta{
     // put update code here
 }
 
 // put new methods here
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+//this will get called every time the player touches the screen
+    [character flap];
+}
+/*-(void)doSomething{
+    NSLog(@"Hello World!");
+}
+-(int)doSomethingWithAString: (NSString*)myString{
+    return [myString length]*10;
+}
 
+ //error on this code
+ (NSArray *)addThisStringToAnArray:(NSString*)firstString(NSString*)secondString:(NSString*)thirdString{
+ return [NSArray arrayWithObjects: firstString, secondString, thirdString, nil];
+ }
+ */
 @end
